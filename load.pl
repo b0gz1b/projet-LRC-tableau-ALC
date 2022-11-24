5c0d93895f376a17588819fd846b2c42fe8de52c
b0gz1b <jules.bouton_popper@etu.sorbonne-universite.fr>
2022-11-24 23:41:42 +0100

    Mise à jour de load.pl

--- load.pl
+++ load.pl
@@ -14,11 +14,5 @@ Lance le programme
 */
 programme :- 
 	premiere_etape(Tbox,Abi,Abr),
-	write('T-Box : '),print(Tbox),
-	nl,write('A-Box concepts: '),print(Abi),
-	nl,write('T-Box roles: '),print(Abr),
-	nl,
 	deuxieme_etape(Abi,Abi1,Tbox),
-	nl,write('A-Box concepts mise a jour: '),print(Abi1),
-	nl,
 	troisieme_etape(Abi1,Abr).
